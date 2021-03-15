e3bf9c1416719fee7bc0795d67410f9b1a0e6b07
Steve Curtis <scurtis7@gmail.com>
2021-03-15 18:49:28 -0400

    add About component

--- src/webapp/src/app/app-routing.module.ts
+++ src/webapp/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from "./components/about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
